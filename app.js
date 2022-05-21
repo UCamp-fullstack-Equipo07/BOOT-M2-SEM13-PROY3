850fccec6bfe29d1ecdb985555a998bdae5915ad
lopez-mgu <lopez.mgu@gmail.com>
2022-05-20 23:56:38 -0600

    electricity render - miguel

--- app.js
+++ app.js
@@ -536,6 +536,7 @@ let countries_api2 = {
 
 const graph01 = document.getElementById('grafica-1');
 const graph02 = document.getElementById('grafica-2');
+const electricityIcons = document.getElementById('electricity');
 const city = document.getElementById('ciudad');
 const container = document.getElementById('bandera-ver');
 
@@ -573,6 +574,20 @@ const getData = async(cityfromSpanish) => {
             </div>
         </div>`
 
+        electricityIcons.innerHTML =`
+        <div class="warning-container-void" id="exercise-void1">
+            <div class="warning-content">
+                <div class="warning-icon">
+                    <svg class="warning-icon-svg" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fill-rule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clip-rule="evenodd"></path>
+                    </svg>
+                </div>
+                <div class="warning-text">
+                    <h3>Pais no encontrado en la base de datos.</h3>
+                </div>
+            </div>
+        </div>`
+
     }else{
         let citytoEnglish = Object.keys(countries)[cityfromSpanish];
         graph01.innerHTML = '<canvas id="myChart"></canvas>';
@@ -580,15 +595,15 @@ const getData = async(cityfromSpanish) => {
         const url = `https://travelbriefing.org/${citytoEnglish === '' ? 'mexico' : citytoEnglish}?format=json`;
         const res = await fetch(url);
         const data = await res.json();
-        console.log(data.maps)
-        setGraph(data.weather);
+        console.log(data.electricity.voltage, data.electricity.frequency, data.electricity.plugs)
+        setGraph(data.weather, data.electricity);
     }
 
 
 
 }
 
-const setGraph = (weather) => {
+const setGraph = (weather, electricity) => {
 
     const wLabels = Object.keys(weather);
     const wTempMax = Object.values(weather).map(item => item.tMin);
@@ -693,7 +708,20 @@ const setGraph = (weather) => {
         document.getElementById('myChartPre'),
         configPreChart
     );
-  
+    
+    console.log(electricity.plugs[0].toLowerCase())
+
+    electricityIcons.innerHTML = `
+        <div id="voltage-frequency">
+            <span>${electricity.voltage + " V"}</span>
+            <span>${electricity.frequency + " Hz"}</span>
+        </div>`;
+
+        electricity.plugs.map(e =>{
+            electricityIcons.innerHTML += `<div id="plug"><img src="https://travelbriefing.org/sites/views/default/images/plugs/${e.toLowerCase()}.svg"></div>`
+        });
+
+        //<div><image><img src="https://travelbriefing.org/sites/views/default/images/plugs/${electricity.plugs[0].toLowerCase()}.svg"></div>
 }
 
 /*Banderas*/
